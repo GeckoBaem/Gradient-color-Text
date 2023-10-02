4a023f7c016a3192af340409e342930d8c90c872
GeckoBaem <geckobaem@gmail.com>
2023-10-02 10:01:07 +0900

    ADD result Print

--- src/Gradient.js
+++ src/Gradient.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import './Gradient.css';
 import './fonts/fonts.css';
-import { toast, ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
+// import { react } from "@babel/types";
+// import { transform } from "@babel/core";
+import { toast, ToastContainer } from "react-toastify"
 // import { type } from "@testing-library/user-event/dist/type";
 
 function Gradient() {
   const [color1, setColor1] = useState('#B993D6');
   const [color2, setColor2] = useState('#8CA6DB');
   const [text, setText] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
+  const [gradient_value, setGradient_value] = useState(null);
   
   const onChange1 = (e) => {
     setColor1(e.target.value);
@@ -20,19 +24,20 @@ function Gradient() {
     setText(e.target.value)
   }
 
+  let hex_value = [];
+
   //HEX 입력값 등간격 계산 
   const EqualInterval = (e) => {
     e.preventDefault();
     const rgb = [1,2,3];
     let decimal_value = [];
-    let hex_value = [];
     let hex_code = "";
     let a = [];
     let x = dataInput(color1);
     let y = dataInput(color2);
 
-    if (!text) {
-      toast.error("Please Enter The Text!")
+    if (text.length <= 1) {
+      toast.error("Please write at least two characters!")
     } else {
       for (let n in rgb) {
         decimal_value.push(colorEqualInterval(x[n], y[n], text.length));  
@@ -46,8 +51,8 @@ function Gradient() {
         }
         hex_value.push(hex_code);
       }
-      console.log(hex_value);
-      
+      setGradient_value(result());
+      setIsVisible(true);
     }
   }
 
@@ -70,11 +75,28 @@ function Gradient() {
     return array;
   }
 
+  //innerHTML 방식 코드 생성
+  function result() {
+    let a = "";
+    let text_array = [...text]
+    for (let i in hex_value) {
+      a += '<span style=\'color:#' + hex_value[i] + '\'>' + text_array[i] + '</span>';
+    }
+    return a;
+  }
+
+  function color_change() {
+    let a = color1;
+    setColor1(color2);
+    setColor2(a);
+  }
+
   return (
     <div>
       <form className="submit" onSubmit={EqualInterval}>
         <div className='gradient-input' 
         style={{backgroundImage:`linear-gradient(to right, ${color1} , ${color2})`}}>
+          <div className="gradient-img" onClick={color_change}></div>
           <input type='color' id="color1" value={color1} onChange={onChange1}></input>
           <input type='color' id="color2" value={color2} onChange={onChange2}></input>
         </div>
@@ -85,6 +107,10 @@ function Gradient() {
         </div>
         <button type='submit'><b>DONE!</b></button>
       </form>
+      {gradient_value&&isVisible!==null&&(<div className="result">
+        <div dangerouslySetInnerHTML={{ __html:gradient_value}}></div>
+        <span id="result-child">{gradient_value}</span>
+      </div>)}
       <ToastContainer
         position="top-center"
         autoClose={2000}
